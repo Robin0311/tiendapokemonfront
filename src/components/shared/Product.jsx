edb76de6c2943a80617168dd533e7e18e2dd5998
Robin9203 <robinxd11@gmail.com>
2023-02-11 02:06:07 +0100

    feat: mas cambios

--- src/components/shared/Product.jsx
+++ src/components/shared/Product.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export default function Product(props) {
   return (
-    <Card className="p-10" style={{ width: '20rem' }}>
-<Card.Img className="product--image" src={props.photoimage} alt="product photoimage" />
-<Card.Body style={{ height: '10rem' }}>
-  <Card.Title>{props.name}</Card.Title>
-  <Card.Subtitle className="mb-2 text-muted">${props.price}</Card.Subtitle>
-  {/* <Card.Text>
+    <Card className="p-10" style={{ width: "20rem" }}>
+      <Card.Img
+        className="product--image"
+        src={props.photoimage}
+        alt="product photoimage"
+      />
+      <Card.Body style={{ height: "10rem" }}>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          ${props.price}
+        </Card.Subtitle>
+        {/* <Card.Text>
   {props.description}
   </Card.Text> */}
-  <Button variant="primary">Add to Cart</Button>
-</Card.Body>
-</Card>
+        <Button variant="primary">Add to Cart</Button>
+      </Card.Body>
+    </Card>
   );
 }
-
